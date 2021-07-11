1098fc6a92cff9e7c76adeb93f6251d638693435
quynhnn-vn <nnquynh.0111@gmail.com>
2021-07-11 13:06:44 +0200

    Modify Contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,41 +1,24 @@
 import React, { useState } from "react";
+import emailjs from "emailjs-com";
+import { apiKey } from "../emailKey";
 
 export function Contact() {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [message, setMessage] = useState("");
-    function encode(data) {
-        return Object.keys(data)
-          .map(
-            (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-          )
-          .join("&");
-      }
-    function sendEmail (templateId, variables) {
-      window.emailjs.send(
-        'gmail', templateId,
-        variables
-        ).then(res => {
-          console.log('Email successfully sent!')
-        })
-        .catch(err => console.error('Oh well, you failed. Here some thoughts on the error that occured:', err))
-    }
-
-    function handleSubmit(e) {
-        const templateId = 'template_cdwbli4';
-        sendEmail(templateId, {message_html: message, from_name: name, reply_to: email})
-    }
-    
-      // function handleSubmit(e) {
-      //   e.preventDefault();
-      //   fetch("/", {
-      //     method: "POST",
-      //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      //     body: encode({ "form-name": "contact", name, email, message }),
-      //   })
-      //     .then(() => alert("Message sent!"))
-      //     .catch((error) => alert(error));
-      // }
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(`gmail`, apiKey.TEMPLATE_ID, e.target, apiKey.USER_ID)
+      .then(
+        (result) => {
+          alert("Message sent, I will get back to you shortly", result.text);
+        },
+        (error) => {
+          alert("An error occurred, Please try again", error.text);
+        }
+      );
+  }
   return (
     <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
@@ -79,12 +62,12 @@ export function Contact() {
           netlify
           name="contact"
           onSubmit={handleSubmit}
-          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
+        >
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Hire Me
           </h2>
-          <p className="leading-relaxed mb-5">
-          </p>
+          <p className="leading-relaxed mb-5"></p>
           <div className="relative mb-4">
             <label htmlFor="name" className="leading-7 text-sm text-gray-400">
               Name
@@ -94,7 +77,7 @@ export function Contact() {
               id="name"
               name="name"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setName(e.target.value)}
+              // onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className="relative mb-4">
@@ -106,25 +89,27 @@ export function Contact() {
               id="email"
               name="email"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setEmail(e.target.value)}
+              // onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="relative mb-4">
             <label
               htmlFor="message"
-              className="leading-7 text-sm text-gray-400">
+              className="leading-7 text-sm text-gray-400"
+            >
               Message
             </label>
             <textarea
               id="message"
               name="message"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setMessage(e.target.value)}
+              // onChange={(e) => setMessage(e.target.value)}
             />
           </div>
           <button
             type="submit"
-            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          >
             Submit
           </button>
         </form>
---
+++ src/emailKey.js
@@ -0,0 +1,4 @@
+export const apiKey = {
+    USER_ID: "user_7ihZXP4joguj6AiK2TPm6",
+    TEMPLATE_ID: "template_0ohgii9",
+}
